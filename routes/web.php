93dc654a36202feafe2f63eb293cd49668096b91
Ahmed a305 <a7med@x000001.ies.com>
2019-03-27 17:36:57 +0300

    v0.01

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+   return view('welcome');
+    });
+
+
+
+
+
+/*
+* Search
+*/
+
+Route::get('/search', [
+    'uses' => '\App\Http\Controllers\SearchController@getResults',
+    'as' => 'search.results'
+]);
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
