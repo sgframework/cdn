c57576c14a7bd47e849410aa77828f373637b591
Ahmed a305 <a7med@x000001.ies.com>
2019-03-31 18:41:10 +0300

    fix_reg

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'idnumber', 'password', 'phonenumber', 'photo',
+        'name', 'email', 'idnumber', 'password', 'phonenumber'
     ];
 
     /**
